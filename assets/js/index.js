bba3d8854839c62f82873f0f5eade4ddf7f2c99a
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2021-10-28 12:48:03 -0500

    mejora de código

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,32 @@
 //const API = "https://rickandmortyapi.com/api/character/"
 const API = "https://jsonplaceholder.typicode.com/users/"
-const xhr = new XMLHttpRequest();
+const HTMLResponse = document.querySelector('#app');
 
-function onRequestHandler(params) {
-    if (this.readyState === 4 && this.status === 200) {
-        const data = JSON.parse(this.response);
-        const HTMLResponse = document.querySelector('#app');
+fetch(`${API}`)
+    .then((response) => response.json())
+    .then((users) => {
+        const tpl = users.map((user) => ` 
+                    <figure figure class= 'hover-img col-md-3 col-12 rounded p-2 ' >
+                        <img class="rounded" src="${img[Math.floor(Math.random() * img.length)]}" />
+                        <figcaption>
+                        <h3>${user.name}</h3>
+                        </figcaption>
+                        <p class="mt-3 h5">📧${user.email} 🙍‍♂️${user.username}</p >
+                    </figure > 
+                    `);
+        HTMLResponse.innerHTML = `${tpl}`;
+    });
 
-        const tpl = data.map((user) => `
-        <figure class='hover-img col-md-3 col-12 rounded p-2 '>
-            <img class="rounded" src="${img[Math.floor(Math.random() * img.length)]}" />
-            <figcaption>
-            <h3>${user.name}</h3>
-            </figcaption>
-            <p class="mt-3 h5">📧${user.email} 🙍‍♂️${user.username}</p >
-
-        </figure >
-        `)
-        HTMLResponse.innerHTML = `${tpl} `;
-        /* console.log(data.info.count); */
-    }
-}
-xhr.addEventListener('load', onRequestHandler);
-xhr.open('GET', `${API} `);
-xhr.send();
+/* fetch(API)
+    .then((response) => response.JSON())
+    .then((users) => {
+        const tpl = users.map((user) =>
+            `<figure figure class= 'hover-img col-md-3 col-12 rounded p-2 ' >
+                        <img class="rounded" src="${img[Math.floor(Math.random() * img.length)]}" />
+                        <figcaption>
+                        <h3>${user.name}</h3>
+                        </figcaption>
+                        <p class="mt-3 h5">📧${user.email} 🙍‍♂️${user.username}</p >
+                    </figure > `);
+        HTMLResponse.innerHTML = `${ tpl } `;
+    }); */
